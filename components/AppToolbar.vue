0d8a1bf0713eeacd7b271a17fdcd267f2197adf2
debugrammer <debugrammer@gmail.com>
2020-06-21 18:44:41 +0900

    Toolbar 메뉴 모바일 뷰 추가

--- components/AppToolbar.vue
+++ components/AppToolbar.vue
@@ -2,21 +2,66 @@
   <client-only>
     <v-app-bar color="secondary" app fixed dark>
       <v-img max-width="135" src="/logo.png" alt="joonsang.com"></v-img>
+
       <v-spacer></v-spacer>
-      <v-btn v-if="$i18n.locale !== 'en'" :to="switchLocalePath('en')" outlined>
-        <v-icon left>mdi-translate</v-icon> English
-      </v-btn>
-      <v-btn
-        v-else-if="$i18n.locale !== 'kr'"
-        :to="switchLocalePath('kr')"
-        outlined
-      >
-        <v-icon left>mdi-translate</v-icon> 한국어
-      </v-btn>
-      <template v-for="(item, i) in menu.external">
-        <v-btn :key="i" :href="item.href" :target="item.target" text icon>
-          <v-icon v-text="item.icon"></v-icon>
+
+      <template v-if="$vuetify.breakpoint.xsOnly">
+        <v-btn
+          v-if="$i18n.locale !== 'en'"
+          :to="switchLocalePath('en')"
+          text
+          icon
+        >
+          <v-icon>mdi-translate</v-icon>
         </v-btn>
+        <v-btn
+          v-else-if="$i18n.locale !== 'kr'"
+          :to="switchLocalePath('kr')"
+          text
+          icon
+        >
+          <v-icon>mdi-translate</v-icon>
+        </v-btn>
+      </template>
+      <template v-else>
+        <v-btn
+          v-if="$i18n.locale !== 'en'"
+          :to="switchLocalePath('en')"
+          outlined
+        >
+          <v-icon left>mdi-translate</v-icon> English
+        </v-btn>
+        <v-btn
+          v-else-if="$i18n.locale !== 'kr'"
+          :to="switchLocalePath('kr')"
+          outlined
+        >
+          <v-icon left>mdi-translate</v-icon> 한국어
+        </v-btn>
+      </template>
+
+      <v-menu v-if="$vuetify.breakpoint.xsOnly" offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-menu-down</v-icon>
+          </v-btn>
+        </template>
+        <v-list flat>
+          <v-list-item-group v-for="(item, i) in menu.external" :key="i">
+            <v-list-item :href="item.href" :target="item.target">
+              <v-list-item>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
+      <template v-else>
+        <template v-for="(item, i) in menu.external">
+          <v-btn :key="i" :href="item.href" :target="item.target" text icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-btn>
+        </template>
       </template>
 
       <v-divider vertical></v-divider>
